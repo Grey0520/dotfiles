c7099173045895cc66e708dee3ff4641d7e09420
Grey0520 <grey0520@foxmail.com>
2023-11-27 00:24:54 +0800

    first

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,19 @@
+local options = {
+  ensure_installed = { 
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "go",
+    "svelte"
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
